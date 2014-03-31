71fea69b0e11e622208e62d0d881bdaa4ada620f
Sebastien Binet <binet@cern.ch>
2014-03-31 09:26:04 +0200

    calo: use geo. add XCentre and YCentre

--- calo-sim/calo.go
+++ calo-sim/calo.go
@@ -1,7 +1,7 @@
 package main
 
 type calogeo struct {
-	ZMin float64
+	ZMin  float64
 	ZMax  float64
 	XYMin float64
 	XYMax float64
@@ -17,31 +17,39 @@ type calogeo struct {
 var Calo calogeo
 
 func init() {
-	Calo.ZMin  = 0.0
-	Calo.ZMax  = 1.0
+	Calo.ZMin = 0.0
+	Calo.ZMax = 1.0
 	Calo.XYMin = -2.0
 	Calo.XYMax = +2.0
 
 	Calo.NbCellsInXY = 40
-	Calo.NbLayers    = 1
+	Calo.NbLayers = 1
 
 	// Cell size in x-y
 	Calo.XYSize = (Calo.XYMax - Calo.XYMin) / float64(Calo.NbCellsInXY)
-	Calo.ZSize  = (Calo.ZMax - Calo.ZMin) / float64(Calo.NbLayers)
+	Calo.ZSize = (Calo.ZMax - Calo.ZMin) / float64(Calo.NbLayers)
 }
 
 func (geo *calogeo) IsInside(x, y, z float64, addr *Cell) bool {
 	*addr = Cell{}
 
-	if x < Calo.XYMin || x > Calo.XYMax ||
-		y < Calo.XYMin || y > Calo.XYMax ||
-		z < Calo.ZMin || z > Calo.ZMax {
+	if x < geo.XYMin || x > geo.XYMax ||
+		y < geo.XYMin || y > geo.XYMax ||
+		z < geo.ZMin || z > geo.ZMax {
 		return false
 	}
 
-	ix := int((x-Calo.XYMin)/Calo.XYSize)+1
-	iy := int((y-Calo.XYMin)/Calo.XYSize)+1
-	iz := int((z-Calo.ZMin)/Calo.ZSize)+1
-	*addr = Cell{X:ix, Y:iy, Layer: iz}
+	ix := int((x-geo.XYMin)/geo.XYSize) + 1
+	iy := int((y-geo.XYMin)/geo.XYSize) + 1
+	iz := int((z-geo.ZMin)/geo.ZSize) + 1
+	*addr = Cell{X: ix, Y: iy, Layer: iz}
 	return true
+}
+
+func (geo *calogeo) XCentre(addr *Cell) float64 {
+	return geo.XYMin + (float64(addr.X-1)+0.5)*geo.XYSize
+}
+
+func (geo *calogeo) YCentre(addr *Cell) float64 {
+	return geo.XYMin + (float64(addr.Y-1)+0.5)*geo.XYSize
 }
